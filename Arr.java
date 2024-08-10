7c2a4d78f25408fc0d59c6fcbada1294442f9aad
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-08-10 10:54:13 +0530

    Add

--- Arr.java
+++ Arr.java
@@ -1,21 +1,62 @@
-class Arr {
-		public int maxProfit(int[] prices) {
-			int profit =0;
-			int store = prices[0];
-			for(int i=0; i<prices.length-1; i++){
-			if(store<prices[i+1]){
-             profit  = profit + prices[i+1]- store;
-			 store = prices[i+1];
-			}
-			else{
-				store = prices[i+1];
-			}
-			}
-			return profit;
+class Node{
+	int data;
+	Node next;
+	Node(int data){
+		this.data = data;
+		this.next = null;
+	}
+}
+class Queue{
+	Node front;
+	Node rare;
+	Queue(){
+      this.front= null;
+	  this.rare = null;
+	}
+	void insert(int data){
+    Node newnode  = new Node(data);
+    if(this.front==null||this.rare==null){
+		this.front=this.rare=newnode;
+	}
+	else{
+		this.rare.next = newnode;
+		this.rare = newnode;
+		this.rare.next = this.front;
+	}
+	}
+	int remove(){
+		if(this.front==null||this.rare==null){
+			return 0;
 		}
-	    public static void main(String[] args) {
-		int [] prices  = {1,2,3,4,5};
-		Arr a = new Arr();
-		System.out.println("total profit + " + a.maxProfit(prices));
+		else{
+		int data = this.front.data;
+         this.front  = this.front.next;
+		 this.rare.next = this.front;
+		 return data;
+		}	
+	}
+	void display(){
+		if(this.front==null||this.rare==null){
+			System.out.println("no element to print");
+		}else{
+           Node temp = this.front;
+		   while (temp.next!=this.front) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		   }
+		   System.out.println(temp.data);
+		}
+	}
+}
+public class Arr {
+	public static void main(String[] args) {
+		Queue q = new Queue();
+		q.insert(10);
+		q.insert(20);
+		q.insert(30);
+		q.insert(40);
+		q.insert(50);
+		q.remove();
+		q.display();
 	}
 }
