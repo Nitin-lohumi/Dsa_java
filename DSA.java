972beeaa2d89f75070f81ead7a44564c0d228e9f
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-10-20 22:58:49 +0530

    Add

--- DSA.java
+++ DSA.java
@@ -1,18 +1,14 @@
-class Solution {
-    public int myAtoi(String s) {
-     long ans = 0;
-     int sign =1; int i =0;
-     while (i<s.length() && s.charAt(i)==' ') i++;
-     if(i<s.length()&&(s.charAt(i)=='-'||s.charAt(i)=='+')){
-        sign = (s.charAt(i)=='-')?-1:1;
-        i++;
+class DSA {
+   public int singleNumber(int[] nums) {
+     int number =0;
+     for(int i =0; i<nums.length; i++){
+      number=number^nums[i];
      }
-     while(i<s.length()&& Character.isDigit(s.charAt(i))){
-        ans = ans*10+(s.charAt(i)-'0');
-        if(ans*sign<=Integer.MIN_VALUE) return Integer.MIN_VALUE;
-        if(ans*sign>= Integer.MAX_VALUE) return Integer.MAX_VALUE;
-        i++;
-     }
-     return (int) ans*sign;
-    }
+      return number;
+   }
+   public static void main(String[] args) {
+      DSA a = new DSA();
+      int arr[]= {3,1,1,2,2};
+      System.out.println(a.singleNumber(arr));
+   }
 }
