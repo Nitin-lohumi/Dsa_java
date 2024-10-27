a84e00e94f905ec8ac9425b6fa1ca67a5fc6debf
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-10-27 21:33:40 +0530

    Add

--- DSA.java
+++ DSA.java
@@ -5,7 +5,6 @@ public class Dsa{
         map.put(1, "Apple");
         map.put(2, "Banana");
         map.put(3, "Orange");
-        map.remove(2);
         for (HashMap.Entry<Integer,String> entry:map.entrySet()) {
             System.out.println("key:" +entry.getKey()+"  "+"value" +entry.getValue());
         }
