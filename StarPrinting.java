ec79c3676879f3dfaa9bb807ddafd07e33b023ba
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-11-06 10:59:19 +0530

    add

--- StarPrinting.java
+++ StarPrinting.java
@@ -5,26 +5,19 @@ public class StarPrinting {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i=1; i<=n; i++) {
-            for (int j=1; j<=i; j++){ 
-                System.out.print("*");
-            } 
-            for (int j = 1; j <=2*(n-i); j++) {
+            for (int j=1; j<=n-i; j++){ 
                 System.out.print(" ");
-            }
-            for (int j = 0; j < i; j++) {
+            } 
+            for (int j = 0; j<(2*i)-1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-
-        for (int i=n; i>=1; i--) {
-            for (int j=1; j<=i; j++){ 
-                System.out.print("*");
-            } 
-            for (int j = 1; j <=(2*(n-i)); j++) {
+        for (int i=n-1; i>=1; i--) {
+            for (int j=1; j<=n-i; j++){ 
                 System.out.print(" ");
-            }
-            for (int j = 1; j <= i; j++) {
+            } 
+            for (int j =0; j<(2*i)-1; j++) {
                 System.out.print("*");
             }
             System.out.println();
