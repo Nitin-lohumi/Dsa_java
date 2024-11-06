f435492936021929d059a9a24c83300a455f858e
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-11-06 10:26:11 +0530

    add

--- StarPrinting.java
+++ StarPrinting.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
 public class StarPrinting {
     public static void main(String[] args) {
-        for (int i = 1; i <= 4; i++) {// row
-            for (int j = 1; j <= 4 - i; j++) { //space 
-                System.out.print("   ");
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=i; j++){ 
+                System.out.print("*");
             } 
-            for (int j = 1; j <= i; j++) {// column printing the stars
-                System.out.print(" * ");
+            for (int j = 1; j <=2*(n-i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i=n; i>=1; i--) {
+            for (int j=1; j<=i; j++){ 
+                System.out.print("*");
+            } 
+            for (int j = 1; j <=(2*(n-i)); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
