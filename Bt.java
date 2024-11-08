87a3f618faf4fd3120e5bfb71a476d0de9ef3c18
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-11-08 22:32:09 +0530

    add

--- Bt.java
+++ Bt.java
@@ -16,10 +16,12 @@ class Node {
 public class Bt {
     Node root;
     int index = -1;
-    int count =0;
+    int count = 0;
+
     Bt() {
         this.root = null;
     }
+
     public Node buildTree(int node[]) {
         index++;
         if (index >= node.length) {
@@ -33,6 +35,7 @@ public class Bt {
         newNode.right = buildTree(node);
         return newNode;
     }
+
     public void inorder(Node node) {
         if (node != null) {
             inorder(node.left);
@@ -40,15 +43,17 @@ public class Bt {
             inorder(node.right);
         }
     }
+
     public int CountNode(Node root) {
         if (root == null) {
             return 0;
         }
-        count =count+1;
+        count = count + 1;
         CountNode(root.left);
         CountNode(root.right);
         return count;
     }
+
     public void BfS_travasal(Node root) {
         if (root == null) {
             System.out.println("no any elements to show ");
@@ -77,36 +82,80 @@ public class Bt {
             }
         }
     }
-    int sum =0;
-    public int sumOfNOde (Node root){
-        if(root==null){
+
+    int sum = 0;
+
+    public int sumOfNOde(Node root) {
+        if (root == null) {
             return 0;
         }
-        // if(root.left==null&&root.right==null){
-        //     return root.data;
-        // }
         int leftsum = sumOfNOde(root.left);
         int rightsum = sumOfNOde(root.right);
-        sum  = leftsum+ rightsum+ root.data;
+        sum = leftsum + rightsum + root.data;
         return sum;
-      }
-      public int FindHeight(Node root){
-            if(root==null){
-            return 0;
-            }
-             int leftheight = FindHeight(root.left);//1
-             int rightHeight = FindHeight(root.right);//1
-             int mainheight = Math.max(leftheight,rightHeight)+1;
-             return mainheight;
-      }
-    public static void main(String[] args) {
-        int[] node = { 1, 2, 3, -1, 9, -1, -1, 3, -1, -1, 4 };
-        Bt t = new Bt();
-        Node root = t.buildTree(node);
-        t.BfS_travasal(root);
-        System.out.println("counting the number of node is "+t.CountNode(root));
-        System.out.println("sum of all nodes are "+ t.sumOfNOde(root));
-        System.out.println("the height of tree is "+ t.FindHeight(root));
     }
 
+    public int FindHeight(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftheight = FindHeight(root.left);// 1
+        int rightHeight = FindHeight(root.right);// 1
+        int mainheight = Math.max(leftheight, rightHeight) + 1;
+        return mainheight;
+    }
+
+    public void levelOrderSum(Node root,int level) {
+        int trackLevel = 0;
+        int sum =0;
+        if(root==null){
+            System.out.println("root is null");
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(null);
+        q.add(root);
+        while (!q.isEmpty()) {
+            Node curr = q.poll();
+            if(curr==null){
+                trackLevel++;
+                 if(q.isEmpty()){
+                    break;
+                 }else{
+                    q.add(null);
+                 }
+            }else{
+                if(curr.left!=null){
+                    q.add(curr.left);
+                } 
+                if(curr.right!=null){
+                    q.add(curr.right);
+                }
+            }
+            if (level == trackLevel) {
+                Node nodeval = q.poll();
+                while(nodeval!=null){
+                    sum = nodeval.data+sum;
+                    nodeval = q.poll();
+                }
+                    System.out.println(sum +" sum of the level");
+                break;
+              }
+              if(FindHeight(root)<level){
+                System.out.println("this is not a level of the treee");
+                return;
+              } 
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] node = { 1, 2, 3, -1, -1, -1, 3, -1, -1, 4 };
+        Bt t = new Bt();
+        Node root = t.buildTree(node);
+        // System.out.println("counting the number of node is " + t.CountNode(root));
+        // System.out.println("sum of all nodes are " + t.sumOfNOde(root));
+        // System.out.println("the height of tree is " + t.FindHeight(root));
+        t.levelOrderSum(root,2);
+        // t.BfS_travasal(root);
+    }
 }
