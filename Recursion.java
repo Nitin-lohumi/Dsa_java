3b0ab6fc8523d47edc476338628982403abc2b81
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-11-13 11:58:45 +0530

    add

--- Recursion.java
+++ Recursion.java
@@ -1,26 +1,25 @@
-import java.util.ArrayList;
 public class Recursion {
-     int fact=0;
-     ArrayList<Integer> arr;
-    Recursion(){
-        this.arr = new ArrayList<Integer>();
-        this.arr.add(0);
-        this.arr.add(1);
+     int frist=-1;
+     int last=-1;
+    public void fun(String source,int i, char ch) {
+       if(i<0){
+        System.out.println(frist +" is in frist occourence ");
+        System.out.println(last +" is in last occourence ");
+        return;
+       }
+       if(source.charAt(i)==ch){
+       if(this.last==-1){
+            this.last = i;
+          }else{
+            this.frist  = i;
+          }
+       }
+       fun(source,i-1,ch);
     }
-    public void fun(int a,int b,int n){
-        if(n==0){
-            return;
-        }
-        int c = a+b;
-        arr.add(c);
-        fun(b, c, n-1);
-    }
+
     public static void main(String[] args) {
         Recursion r = new Recursion();
-        int a =0;
-        int b =1;
-        int n =10;
-        r.fun(a, b, n);
-        System.out.println(r.arr);
+        r.fun("abcddddaaah",10,'a');
+
     }
 }
